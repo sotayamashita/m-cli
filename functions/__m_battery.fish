0cbc0a1d51b18f1e8592a9996b073a80b37a401a
Sota Yamashita <sota.yamashita@gmail.com>
2016-07-31 23:24:04 +0900

    Refactering battery shell

--- functions/__m_battery.fish
+++ functions/__m_battery.fish
@@ -3,29 +3,25 @@ function __battery_status
 end
 
 function __battery_help
-    set -l uline (set_color -u)
-    set -l nc (set_color normal)
-
     echo "Usage: m battery [COMMAND]"
     echo
     echo "where COMMAND can be one of:"
     echo "       status  Get the battery status"
     echo "       help"
-    echo
-    echo "Examples:"
-    echo "       m battery status # get the battery status"
 end
 
 function __m_battery
     switch "$argv[1]";
         case help
-            set -e $argv[1]
             __battery_help
             return
 
         case status
-            set -e $argv[1]
             __battery_status
             return
+
+        case \*
+            __battery_help
+
     end
 end
